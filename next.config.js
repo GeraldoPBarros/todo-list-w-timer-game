b4c017dd6f7bb0d1ab2d8bc1342880a98a673677
BarrosNB <69353700+BarrosNB@users.noreply.github.com>
2024-06-30 18:23:54 -0300

    pwa functionality.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+});
 
-module.exports = nextConfig
+module.exports = withPWA({
+  nextConfig,
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default class MyDocumento extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
           <link rel="shortcut icon" href="/PageIcon_b.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
